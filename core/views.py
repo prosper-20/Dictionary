ab4bad6da1bbd696d67e150b56dd950bda47ba3d
prosper-20 <edwardprosper001@gmail.com>
2022-07-07 09:55:18 +0100

    updates

--- core/views.py
+++ core/views.py
@@ -66,7 +66,84 @@ def HomeView(request):
 
 
 def SearchView(request):
+    word = request.GET['word']
+
+    response = requests.get('https://www.dictionary.com/browse/'+word)
+    response2 = requests.get('https://www.thesaurus.com/browse/'+word)
+
+    if response:
+        soup_1 = bs4.BeautifulSoup(response.text, 'lxml')
+
+        meaning = soup_1.find_all('div', {'value': '1'})
+        meaning_1 = meaning[0].getText()
+    else:
+        word = f"Sorry we couldn't find your word {word} in our records."
+        meaning = ''
+        meaning_1 = ''
+
+    if response2:
+        soup_2 = bs4.BeautifulSoup(response2.text, 'lxml')
+
+        synonyms = soup_2.find_all('a', {'class': 'css-r5sw71-ItemAnchor etbu2a31'})
+        synonym_list = []
+        for b in synonyms[0:]:
+            re = b.text.strip()
+            synonym_list.append(re)
+        main_synonym_list = synonym_list
+
+    
     return render(request, 'mydictionary/serach.html')
+
+
+def word(request):
+    
+    word = request.GET['word']
+
+    res = requests.get('https://www.dictionary.com/browse/'+word)
+    res2 = requests.get('https://www.thesaurus.com/browse/'+word)
+    
+
+    if res:
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+        meaning = soup.find_all('div', {'value': '1'})
+        meaning1 = meaning[0].getText()
+    else:
+        word = 'Sorry, '+ word + ' Is Not Found In Our Database'
+        meaning = ''
+        meaning1 = ''
+
+    if res2:
+        soup2 = bs4.BeautifulSoup(res2.text, 'lxml')
+
+        synonyms = soup2.find_all('a', {'class': 'css-r5sw71-ItemAnchor etbu2a31'})
+        ss = []
+        for b in synonyms[0:]:
+            re = b.text.strip()
+            ss.append(re)
+        se = ss
+        
+
+        
+
+        antonyms = soup2.find_all('a', {'class': 'css-lqr09m-ItemAnchor etbu2a31'})
+        aa = []
+        for c in antonyms[0:]:
+            r = c.text.strip()
+            aa.append(r)
+        ae = aa
+    else:
+        se = ''
+        ae = ''
+
+
+    results = {
+        'word' : word,
+        'meaning' : meaning1,
+    }
+
+
+    return render(request, 'word.htm', {'se': se, 'ae': ae, 'results': results})
 
 
 
